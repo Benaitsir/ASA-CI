34ee642213d2c08d2372b0677abb007aad1431c5
Benaitsir <106411829+Benaitsir@users.noreply.github.com>
2024-08-16 15:45:58 +0000

    Version Complète de création des bases de compilation

--- Fonction_Fanaf_bases.R
+++ Fonction_Fanaf_bases.R
@@ -5,7 +5,7 @@ comp_Fanaf_bases <- function(branche, annee, etat) {
   # Packages ----
   require(tidyverse) # Traitement de données
   require(openxlsx) # chargement docs excel
-  
+  # Conditions ----
   if (branche == "VIE") {
     # Chargement des états de base pour la compilation ----
     if (etat == "Bilan") {
@@ -114,98 +114,120 @@ comp_Fanaf_bases <- function(branche, annee, etat) {
       saveRDS(base_C11_Vie, "5 - Scripts/Fanaf_C11_Vie.RDS")
     }
     
+    else {print(cat("Veuillez choisir un etat répertorié dans la liste :\n 'Bilan', 'CEG', 'C1', 'C4', 'C5', 'C11'"))}
   }
   
-  
-  ## BILAN NON VIE
-  base_Bil_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "BILAN NON VIE")
-  ### On indique l'annee concernée
-  (c <- colnames(base_Bil_N.Vie)) 
-  c[which(c == "2023")] <- annee
-  colnames(base_Bil_N.Vie) <- c
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_Bil_N.Vie)) {
-    for (j in 1:ncol(base_Bil_N.Vie)) {
-      if (is.na(base_Bil_N.Vie[i,j] == TRUE)) {base_Bil_N.Vie[i,j] <- 0}
+  else if (branche == "IARD") {
+    # Chargement des états de base pour la compilation ----
+    if (etat == "Bilan") {
+      ## BILAN NON VIE
+      base_Bil_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "BILAN NON VIE")
+      ### On indique l'annee concernée
+      (c <- colnames(base_Bil_N.Vie)) 
+      c[which(c == "2023")] <- annee
+      colnames(base_Bil_N.Vie) <- c
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_Bil_N.Vie)) {
+        for (j in 1:ncol(base_Bil_N.Vie)) {
+          if (is.na(base_Bil_N.Vie[i,j] == TRUE)) {base_Bil_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(c,i,j)
+      ### Sauvegarde
+      saveRDS(base_Bil_N.Vie, "5 - Scripts/Fanaf_Bilan_N.Vie.RDS")
     }
-  }
-  rm(c,i,j)
-  
-  ## CEG NON VIE
-  base_CEG_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "CEG NON VIE")
-  ### On indique l'annee concernée
-  base_CEG_N.Vie[1, ncol(base_CEG_N.Vie)] <- annee
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_CEG_N.Vie)) {
-    for (j in 1:ncol(base_CEG_N.Vie)) {
-      if (is.na(base_CEG_N.Vie[i,j] == TRUE)) {base_CEG_N.Vie[i,j] <- 0}
+    
+    else if (etat == "CEG") {
+      ## CEG NON VIE
+      base_CEG_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "CEG NON VIE")
+      ### On indique l'annee concernée
+      base_CEG_N.Vie[1, ncol(base_CEG_N.Vie)] <- annee
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_CEG_N.Vie)) {
+        for (j in 1:ncol(base_CEG_N.Vie)) {
+          if (is.na(base_CEG_N.Vie[i,j] == TRUE)) {base_CEG_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(i,j)
+      ### Sauvegarde
+      saveRDS(base_CEG_N.Vie, "5 - Scripts/Fanaf_CEG_N.Vie.RDS")
     }
-  }
-  rm(i,j)
-  
-  ## C1 NON VIE
-  base_C1_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C1 NON VIE")
-  ### On indique l'annee concernée
-  (c <- colnames(base_C1_N.Vie)) 
-  c[which(c == "2023")] <- annee
-  colnames(base_C1_N.Vie) <- c
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_C1_N.Vie)) {
-    for (j in 1:ncol(base_C1_N.Vie)) {
-      if (is.na(base_C1_N.Vie[i,j] == TRUE)) {base_C1_N.Vie[i,j] <- 0}
+    
+    else if (etat == "C1") {
+      ## C1 NON VIE
+      base_C1_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C1 NON VIE")
+      ### On indique l'annee concernée
+      (c <- colnames(base_C1_N.Vie)) 
+      c[which(c == "2023")] <- annee
+      colnames(base_C1_N.Vie) <- c
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_C1_N.Vie)) {
+        for (j in 1:ncol(base_C1_N.Vie)) {
+          if (is.na(base_C1_N.Vie[i,j] == TRUE)) {base_C1_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(c,i,j)
+      ### Sauvegarde
+      saveRDS(base_C1_N.Vie, "5 - Scripts/Fanaf_C1_N.Vie.RDS")
     }
-  }
-  rm(c,i,j)
-  
-  ## C4 NON VIE
-  base_C4_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C4 NON VIE")
-  ### On indique l'annee concernée
-  (c <- colnames(base_C4_N.Vie)) 
-  c[which(c == "2023")] <- annee
-  colnames(base_C4_N.Vie) <- c
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_C4_N.Vie)) {
-    for (j in 1:ncol(base_C4_N.Vie)) {
-      if (is.na(base_C4_N.Vie[i,j] == TRUE)) {base_C4_N.Vie[i,j] <- 0}
+    
+    else if (etat == "C4") {
+      ## C4 NON VIE
+      base_C4_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C4 NON VIE")
+      ### On indique l'annee concernée
+      (c <- colnames(base_C4_N.Vie)) 
+      c[which(c == "2023")] <- annee
+      colnames(base_C4_N.Vie) <- c
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_C4_N.Vie)) {
+        for (j in 1:ncol(base_C4_N.Vie)) {
+          if (is.na(base_C4_N.Vie[i,j] == TRUE)) {base_C4_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(c,i,j)
+      ### Sauvegarde
+      saveRDS(base_C4_N.Vie, "5 - Scripts/Fanaf_C4_N.Vie.RDS")
     }
-  }
-  rm(c,i,j)
-  
-  ## C5 NON VIE
-  base_C5_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C5 NON VIE SYNTH")
-  ### On indique l'annee concernée
-  (c <- colnames(base_C5_N.Vie)) 
-  c[which(c == "2023")] <- annee
-  colnames(base_C5_N.Vie) <- c
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_C5_N.Vie)) {
-    for (j in 1:ncol(base_C5_N.Vie)) {
-      if (is.na(base_C5_N.Vie[i,j] == TRUE)) {base_C5_N.Vie[i,j] <- 0}
+    
+    else if (etat == "C5") {
+      ## C5 NON VIE
+      base_C5_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C5 NON VIE SYNTH")
+      ### On indique l'annee concernée
+      (c <- colnames(base_C5_N.Vie)) 
+      c[which(c == "2023")] <- annee
+      colnames(base_C5_N.Vie) <- c
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_C5_N.Vie)) {
+        for (j in 1:ncol(base_C5_N.Vie)) {
+          if (is.na(base_C5_N.Vie[i,j] == TRUE)) {base_C5_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(c,i,j)
+      ### Sauvegarde
+      saveRDS(base_C5_N.Vie, "5 - Scripts/Fanaf_C5_N.Vie.RDS")
     }
-  }
-  rm(c,i,j)
-  
-  ## C11 NON VIE
-  base_C11_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C11 NON VIE")
-  ### On indique l'annee concernée
-  base_C11_N.Vie[1, ncol(base_C11_N.Vie)] <- annee
-  base_C11_N.Vie[4, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
-  base_C11_N.Vie[20, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
-  base_C11_N.Vie[36, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
-  ### Remplacement des 'NA' par 0
-  for (i in 1:nrow(base_C11_N.Vie)) {
-    for (j in 1:ncol(base_C11_N.Vie)) {
-      if (is.na(base_C11_N.Vie[i,j] == TRUE)) {base_C11_N.Vie[i,j] <- 0}
+    
+    else if (etat == "C11") {
+      ## C11 NON VIE
+      base_C11_N.Vie <- openxlsx::read.xlsx("3 - FANAF/Modele FANAF.xlsx", sheet = "C11 NON VIE")
+      ### On indique l'annee concernée
+      base_C11_N.Vie[1, ncol(base_C11_N.Vie)] <- annee
+      base_C11_N.Vie[4, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
+      base_C11_N.Vie[20, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
+      base_C11_N.Vie[36, c(3, 4, 5)] <- c(annee - 2, annee - 1, annee)
+      ### Remplacement des 'NA' par 0
+      for (i in 1:nrow(base_C11_N.Vie)) {
+        for (j in 1:ncol(base_C11_N.Vie)) {
+          if (is.na(base_C11_N.Vie[i,j] == TRUE)) {base_C11_N.Vie[i,j] <- 0}
+        }
+      }
+      rm(i,j)
+      ### Sauvegarde
+      saveRDS(base_C11_N.Vie, "5 - Scripts/Fanaf_C11_N.Vie.RDS")
     }
+    
+    else {print(cat("Veuillez choisir un etat répertorié dans la liste :\n 'Bilan', 'CEG', 'C1', 'C4', 'C5', 'C11'"))}
   }
-  rm(i,j)
   
-  # Sauvegarde des documents ----
-  
-  saveRDS(base_Bil_N.Vie, "5 - Scripts/Fanaf_Bilan_N.Vie.RDS")
-  saveRDS(base_CEG_N.Vie, "5 - Scripts/Fanaf_CEG_N.Vie.RDS")
-  saveRDS(base_C1_N.Vie, "5 - Scripts/Fanaf_C1_N.Vie.RDS")
-  saveRDS(base_C4_N.Vie, "5 - Scripts/Fanaf_C4_N.Vie.RDS")
-  saveRDS(base_C5_N.Vie, "5 - Scripts/Fanaf_C5_N.Vie.RDS")
-  saveRDS(base_C11_N.Vie, "5 - Scripts/Fanaf_C11_N.Vie.RDS")
+  else{print(cat("Veuillez choisir une branche répertoriée dans la liste :\n 'IARD', 'VIE'"))}
 }
